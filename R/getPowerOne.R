f5f5a0d62ecbe4d2da3b7d06570aaffb668464c9
redaq <redaq@channing.harvard.edu>
2020-07-21 16:29:34 -0400

    Updated getPower function Updated vignettes file

--- R/getPowerOne.R
+++ R/getPowerOne.R
@@ -1,7 +1,7 @@
 getPowerOne <- function(resultAll, method="fdr", significance=0.05)	
 {		#### collect results
 		message("\nComputing the power/type I error rate for the dataset using ", method, " at ", significance, ": \n")
-		resultsAlladj = apply(as.matrix(resultAll[,-c(1:2)]), 2, function(x) p.adjust(x, method=method))
+		resultsAlladj = apply(as.matrix(resultAll$resultAll[,-c(1:2)]), 2, function(x) p.adjust(x, method=method))
 		power = apply(as.matrix(resultsAlladj), 2, function(x) sum(x<significance & x>=0, na.rm=T)/sum(!is.na(x)&x>=0, na.rm=T))
 		print(power)
 		return((power))
